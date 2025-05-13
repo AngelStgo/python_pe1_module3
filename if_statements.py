3bd48a84afca3f27d4cc575ab6738b9699c9a1f5
AngelStgo <euangel.stgo@gmail.com>
2025-05-13 17:03:04 -0400

    ...

--- if_statements.py
+++ if_statements.py
@@ -2,7 +2,7 @@
 #               else do something else
 
 print("Please comfirm your age before signing in.")
-age = int(input("Enter your age"))
+age = int(input("I demand you to enter your age: "))
 
 
 if age  >= 100:
@@ -16,3 +16,18 @@ elif age < 0:
 
 else:
     print("You need to be older than 18 to sign in!")
+
+
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+ 
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+ 
+# Print the result
+print("The larger number is:", larger_number)
+ 
