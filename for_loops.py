69507411a1c964b4a839a5cab6eeeb1f65d5dd6c
AngelStgo <euangel.stgo@gmail.com>
2025-05-17 22:22:28 -0400

    more for-in  examples

--- for_loops.py
+++ for_loops.py
@@ -1,5 +1,20 @@
 
 
+# for in range
+
+for i in range(10):
+    print("The value of i is currently", i)
+    
+    
+for e in range(2, 8):
+    print("The value of i is currently", i)
+ 
+ 
+for k in range(2, 8, 3):
+    print("The value of i is currently", i)
+
+
+
 successful = True 
 for number in range(3):
     print("Attempt")
@@ -8,3 +23,10 @@ for number in range(3):
         break
 else:
     print("Attempt 3 times and failed")
+    
+    
+# nested loops 
+    
+    for x in range(5):
+        for y in range(3):
+            print(f"({x}, {y})")
